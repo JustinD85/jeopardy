c655512e14711f3ab688de830dd93d1b79a49a4b
Justin <justin@baal-software.com>
2018-12-04 19:29:32 -0700

    Add Category and clue column 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
 import { Game } from './game.js';
+import { Player } from './player.js';
 const game = new Game();
 
-game.start();
+//Notionally from DOM
+const playerOneName = 'Travis';
+const playerTwoName = 'TRobot';
+const playerThreeName = 'TJustins';
+const boardFromDOM = document.querySelectorAll('.game-board');
+
+//create users array
+var playerArray =
+  [
+    new Player(playerOneName),
+    new Player(playerTwoName),
+    new Player(playerThreeName)
+  ];
+
+game.start(playerArray);
+game.setUpBoard(boardFromDOM);
