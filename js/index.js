036af062dd6d2c7f6e47800af0d2576274aae346
Travis Gee <geet084@gmail.com>
2018-12-06 19:06:03 -0700

    Add event listener for clicking clues but not categories

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const boardFromDOM = document.querySelectorAll('.game-board');
 let game;
 
 //create users array
-get('#start-btn').addEventListener('click', function () {
+get('#start-btn').addEventListener('click', () => {
   // playerOneName = get('#p1-name-input').value;
   // playerTwoName = get('#p2-name-input').value;
   // playerThreeName = get('#p3-name-input').value;
@@ -22,18 +22,15 @@ get('#start-btn').addEventListener('click', function () {
 
   window.setTimeout(() => {
     game = new Game([playerOneName, playerTwoName, playerThreeName]);
-  }, 500)
+  }, 500) 
+});
 
- 
-
-  // get('.player1-name-label').innerText = playerOneName;
-  // get('.player2-name-label').innerText = playerTwoName;
-  // get('.player3-name-label').innerText = playerThreeName;
-
-  // get('#p1-name-input').value = '';
-  // get('#p2-name-input').value = '';
-  // get('#p3-name-input').value = '';
-  
+get('#view').addEventListener('click', () => {
+  let target = event.target.dataset.id;
+  if (target) {
+    game.updatePlayerScore(target)
+    game.render();
+  }
 });
 
 //EVENT BASED GAME!!, events activate when we click something!
