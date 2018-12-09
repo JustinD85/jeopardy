c1c9a6d2c86fd960e2b751c384e1098377ae2482
Travis Gee <geet084@gmail.com>
2018-12-08 17:25:45 -0700

    WIP - add dom interaction @justinD85 #33

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ import {get, getAll, createEl} from './util/utilities.js'
 let game;
 const domMethods = {
   render,
-  selectClue,
+  showAnswers,
   createBoard,
   createPlayerArea,
   createQuitButton,
@@ -13,7 +13,10 @@ const domMethods = {
   clearScreen,
   transitionToGame
 }
-
+/*
+  NEED TO USE GAME.UPDATE ONLY TO UPDATE GAME
+*/
+console.error('NEED TO USE GAME.UPDATE ONLY TO UPDATE GAME')
 get('#start-btn').addEventListener('click', transitionToGame );
 // get('#game-board').addEventListener('click', render);
 
@@ -21,31 +24,152 @@ function render(event) {
   let target = event.target;
 
   if (target.closest('.clue') && target.closest('.clue').dataset.id) {
+
     if (target.dataset.id) {
       let clueId = target.dataset.id;
-      selectClue(clueId);
+      
+      if (game.data[clueId].isDailyDouble) {
+        showWager(clueId);
+        
+      } else {
+        showAnswers(clueId);
+      }
     }
+
+
+
   }
 
   if (target.closest('.answerContainer')) {
     let clueId = target.closest('.clue').dataset.id;
-
-    game.update(clueId, target.innerText)
-
-    //update score
-
-
+    const playerArea = get('#player-area');
+    game.update(clueId, target.innerText);
+    clearPlayerArea();
+    updatePlayers(playerArea);
+    
   }
 }
 
-function selectClue(clueId) {
+function showWager(clueId) {
+  let wagerContainer;
+  let tempElement;
+  let tempBox;
+
+  let clueContainer = get(`.clue[data-id="${clueId}"]`);
+
+  tempElement = createEl('h1');
+  tempElement.innerText = 'Wager';
+  tempElement.classList.add('daily-double')
+  wagerContainer = createEl('div');
+  wagerContainer.classList.add('wager');
+  wagerContainer.append(tempElement);
+
+  tempElement = createEl('section');
+
+  tempBox = createEl('span');
+  tempBox.classList.add('number');
+  tempBox.innerText = '5';
+  tempElement.append(tempBox);
+
+  tempBox = createEl('span');
+  tempBox.classList.add('number');
+  tempBox.innerText = '10';
+  tempElement.append(tempBox);
+
+  tempBox = createEl('span');
+  tempBox.classList.add('number');
+  tempBox.innerText = '100';
+  tempElement.append(tempBox);
+
+  tempBox = createEl('span');
+  tempBox.classList.add('number');
+  tempBox.innerText = '1000';
+  tempElement.append(tempBox);
+  wagerContainer.append(tempElement);
+
+  tempElement = createEl('section');
+
+  tempBox = createEl('span');
+  tempBox.classList.add('number');
+  tempBox.innerText = '-5';
+  tempElement.append(tempBox);
+
+  tempBox = createEl('span');
+  tempBox.classList.add('number');
+  tempBox.innerText = '-10';
+  tempElement.append(tempBox);
+
+  tempBox = createEl('span');
+  tempBox.classList.add('number');
+  tempBox.innerText = '-100';
+  tempElement.append(tempBox);
+
+
+  tempBox = createEl('span');
+  tempBox.classList.add('number');
+  tempBox.innerText = '-1000';
+  tempElement.append(tempBox);
+  wagerContainer.append(tempElement);
+
+
+  tempElement = createEl('section');
+
+  tempBox = createEl('span');
+  tempBox.classList.add('wager-range');
+  tempBox.innerText = 'min: 5';
+  tempElement.append(tempBox);
+  wagerContainer.append(tempElement);
+
+  tempBox = createEl('button');
+  tempBox.id = 'wager-amount';
+  tempBox.innerText = '0';
+  tempElement.append(tempBox);
+  wagerContainer.append(tempElement);
+
+
+  tempBox = createEl('span');
+  tempBox.classList.add('wager-range');
+  tempBox.innerText = 'max: 9999';
+  tempElement.append(tempBox);
+  wagerContainer.append(tempElement);
+
+
+
+  
+  
+  
+  clueContainer.innerHTML = '';
+  clueContainer.append(wagerContainer);
+  get('#wager-amount').addEventListener('click', () => {
+    clueContainer.innerHTML = '';
+    showAnswers(clueId)
+  });
+  // looking to add interaction??
+  [...getAll('.number')].forEach((wagerNum) => {
+    wagerNum.addEventListener('click', (e) => {
+      const selectedAmt = parseInt(e.target.innerText);
+      const submitAmount = parseInt(get('#wager-amount').innerText);
+      const newAmount = submitAmount + selectedAmt;
+      get('#wager-amount').innerText = newAmount;
+      game.data[clueId].value = newAmount;
+    }) 
+  })
+    
+    
+}
+
+function showAnswers(clueId) {
   let clue = get(`.clue[data-id="${clueId}"]`);
   let answerContainer;
   let answerElement;
   let continueBtn;
   let answer = game.data[clueId].answer;
-  let cat = game.data[clueId].category;
 
+  // will need this when questions are full screen
+  // let cat = game.data[clueId].category;
+
+
+  
   // let question = game.data[clueId].question;
 
   // get(`.clue[data-id="${clueId}"]`).innerHTML = question;
@@ -106,17 +230,24 @@ function createBoard() {
 }
 
 function createPlayerArea() {
-  let playerList = createEl('section');
-  playerList.classList.add('player-area');
+  let playerArea = createEl('section');
+  playerArea.id = 'player-area';
+  get('#view').append(playerArea);
+  updatePlayers(playerArea);
+}
+
+function updatePlayers(playerArea) {
   
   game.players.forEach((player, i) => {
     let user = createEl('article');
     let { name, score } = player;
     user.classList.add(`player-${i}`);
     user.innerText = `${name} score: ${score}`;
-    playerList.append(user);
+    playerArea.append(user);
   });
-  get('#view').append(playerList);
+}
+function clearPlayerArea() {
+  get('#player-area').innerHTML = '';
 }
 
 function createQuitButton() { 
@@ -134,11 +265,19 @@ function resetGame() {
 function clearScreen() {
   get('#view').innerHTML = '';
 }
-
-function transitionToGame() {
+function showGame() {
+  
+}
+function showPlayerScores() {
   const playerOneName = get('#p1-name-input').value;
   const playerTwoName = get('#p2-name-input').value;
   const playerThreeName = get('#p3-name-input').value;
+
+  return [playerOneName, playerTwoName, playerThreeName];
+}
+
+function transitionToGame() {
+  const [playerOneName, playerTwoName, playerThreeName] = showPlayerScores();
 
   // transition img below
   get('#view').innerHTML = '<img src="./img/loading.gif">';
