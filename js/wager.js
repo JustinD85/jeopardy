ff852255a07e8a79724360fe5a2a236c1a61d3d0
Travis Gee <geet084@gmail.com>
2018-12-08 17:12:18 -0700

    Extend clue and add the ability to alter the clue value

--- js/wager.js
+++ js/wager.js
@@ -1,3 +1,13 @@
-export class Wager {
+import { Clue } from './clue.js';
+
+export class Wager extends Clue {
+  constructor(question, answer, pointValue, category, isDailyDouble) {
+    super(question, answer, pointValue, category);
+    this.isDailyDouble = isDailyDouble;
+  }
   
+  updateValue(num) {
+    this.value = num;
+  }
+
 }
