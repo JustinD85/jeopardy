70c46a0e60aac96fba4eab40769f59eb0cecf064
Travis Gee <geet084@gmail.com>
2018-12-08 17:16:26 -0700

    Flesh out clue class #17 #19 #21 #22

--- js/clue.js
+++ js/clue.js
@@ -1,3 +1,10 @@
 export class Clue {
-
+  constructor(question, answer, pointValue, category) {
+    this.question = question;
+    this.answer = answer;
+    this.available = true;
+    this.value = pointValue;
+    this.category = category;
+  }
+  
 }
