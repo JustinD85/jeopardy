5501e9280e9e0ad5b326f8a9699bf2ca75bbcbc1
Justin <justin@baal-software.com>
2018-12-10 15:22:58 -0700

    Give board behaviour

--- js/board.js
+++ js/board.js
@@ -2,9 +2,32 @@ class Board {
   constructor() {
     
   }
-  populate() {
-    
+  createBoard() {
+    let tempGameBoard = createEl('main');
+    tempGameBoard.id = 'game-board';
+    for (let i = 0, id = 0; i < 5; i++) {
+      let column = createEl('section');
+      column.classList.add('category');
+      for (let j = 0; j < 5; j++) {
+        let row = createEl('article');
+        if (j === 0) {
+          row.classList.add('clue');
+          row.innerHTML = `<h1>${game.data[id].category}</h1>`;
+        }
+        else {
+          row.classList.add('clue');
+          row.dataset.id = `${id}`;
+          row.innerHTML = `<h1> ${game.data[id].value}</h1>`;
+          id++;
+        }
+        column.append(row);
+      }
+      tempGameBoard.append(column);
+    }
+    return tempGameBoard;
   }
 }
 
-module.exports = Board;
+if (typeof module !== 'undefined') {
+  module.exports = Board;
+}
