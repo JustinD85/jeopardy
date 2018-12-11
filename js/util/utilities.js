3acb315ea40329689ef7dea03312290247ca13a4
Travis Gee <geet084@gmail.com>
2018-12-11 10:00:41 -0700

    Refactor createEl functions for readability

--- js/util/utilities.js
+++ js/util/utilities.js
@@ -1,32 +1,34 @@
-function get(Element) {
-  return document.querySelector(Element);
+function createElWithId(ele, elName, elText, htmlText) {
+
+  let tempElement = document.createElement(ele);
+  elName = elName.substr(1);
+
+  tempElement.id = elName;
+
+  return addTextOrHTML(tempElement, elText, htmlText);
 }
 
-function getAll(Element) {
-  return document.querySelectorAll(Element);
+function createElWithClass(ele, elName, elText, htmlText) {
+
+  let tempElement = document.createElement(ele);
+  elName = elName.substr(1);
+
+  tempElement.classList.add(elName);
+  
+  return addTextOrHTML(tempElement, elText, htmlText);
 }
 
-function createElementWith(element, elementName, elementText, htmlText) {
+function addTextOrHTML(tempElement, elText, htmlText) {
 
-  let tempElement = document.createElement(element);
-  let elSelectorType = elementName.split('');
-  let type = elSelectorType.shift();
-  elSelectorType = elSelectorType.join('');
-
-  if (type === '.') {
-    tempElement.classList.add(elSelectorType); 
-  }
-  if (type === '#') {
-    tempElement.id = elSelectorType;
-  }
-  if (elementText && elementText !== undefined && elementText !== '') {
-    tempElement.innerText = elementText;
+  if (elText && elText !== undefined && elText !== '') {
+    tempElement.innerText = elText;
   }
   if (htmlText && htmlText !== undefined && htmlText !== '') {
     tempElement.innerHTML = htmlText;
   }
   return tempElement;
 }
+
 if (typeof module !== 'undefined') {
-  module.exports = { get, getAll, createElementWith };
+  module.exports = { createElWithId, createElWithClass, addTextOrHTML };
 }
