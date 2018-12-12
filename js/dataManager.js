e781c94b4ebacc072a88bcde737813bce73bdb81
Justin <justin@baal-software.com>
2018-12-11 22:19:24 -0700

    Refactor further, dataManger js, final round is random now all points

--- js/dataManager.js
+++ js/dataManager.js
@@ -22,60 +22,62 @@ class DataManager {
     }
     return tempArray;
   }
-
-  createMatrixFromData(maxPointValue, randomCategoriesKeys) {
-    let count = 100;
-    let tempCats;
+  
+  getCluesForBoard(randomCategoriesKeys) {
     let arrayOfClueByPointValues = [];
     randomCategoriesKeys.pop(); // so we get 9 cats
-    while (count <= maxPointValue) {
+    
+    randomCategoriesKeys.forEach((categoryKey) => {
 
-      tempCats = randomCategoriesKeys.map((categoryKey) => {
-        let log = this.data.clues.filter((clue) => {
-          return clue.categoryId === this.data.categories[categoryKey] &&
-            clue.pointValue === count;
-        })
-        return log[0];
+      let allCluesThisCat = this.data.clues.filter((clue) => {
+        return clue.categoryId === this.data.categories[categoryKey];
       })
-      count += 100;
-      arrayOfClueByPointValues.push(tempCats);
-    }
+
+      allCluesThisCat = this.randomizeArray(allCluesThisCat);
+      arrayOfClueByPointValues.push(allCluesThisCat.find((clue) => clue.pointValue === 100));
+      arrayOfClueByPointValues.push(allCluesThisCat.find((clue) => clue.pointValue === 200));
+      arrayOfClueByPointValues.push(allCluesThisCat.find((clue) => clue.pointValue === 300));
+      arrayOfClueByPointValues.push(allCluesThisCat.find((clue) => clue.pointValue === 400));
+    })
+
+    arrayOfClueByPointValues.splice(36);
     return arrayOfClueByPointValues;
   }
+
   generateDailyDoubleNums() {
     const dailyDoubles = [];
     dailyDoubles.push(this.randomNumber(0, 16));
     dailyDoubles.push(this.randomNumber(16, 24));
     dailyDoubles.push(this.randomNumber(24, 33));
-    dailyDoubles.push(this.randomNumber(32, 33));
+    dailyDoubles.push(this.randomNumber(32, 36));
+
     return dailyDoubles;
   }
   randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
+
   formatData() {
     const notRandomCategories = Object.keys(this.data.categories);
     const randomCategoriesKeys = this.randomizeArray(notRandomCategories);
+    const flippedTuringCategories = this.flipDataSetValues(this.data.categories);
+    const dailyDoubles = this.generateDailyDoubleNums();
 
-    return this.createMatrixFromData(400, randomCategoriesKeys).reduce((acc, pointValueArray, i) => {
-      let clueId = (100 * i) / 100;
-
-      pointValueArray.forEach((clue, i) => {
-        let currentCategory = randomCategoriesKeys[i];
-        let { question, answer, pointValue } = clue;
-        let category = this.parseTitle(currentCategory);
-        let dailyDoubles = this.generateDailyDoubleNums();
-
-        if (dailyDoubles.includes(clueId)) {
-          acc[clueId] = new Wager(question, answer, pointValue, category);
-        } else {
-          acc[clueId] = new Clue(question, answer, pointValue, category);
-        }
-        clueId += 4;
-      })
-
+    return this.getCluesForBoard(randomCategoriesKeys).reduce((acc, clue, clueIdForDOM) => {
+      let currentCategory = flippedTuringCategories[clue.categoryId];
+      let clueOptions = {
+        question: clue.question,
+        answer: clue.answer,
+        pointValue: clue.pointValue,
+        category: this.parseTitle(currentCategory)
+      };
+      
+      if (clueIdForDOM > 31 && !dailyDoubles.includes(clueIdForDOM)) return acc; 
+      if (dailyDoubles.includes(clueIdForDOM)) { acc.push(new Wager(clueOptions)) }
+      else { acc.push(new Clue(clueOptions)) }
+      
       return acc;
-    }, {});
+    }, []);
   }
 
   parseTitle(str) {
